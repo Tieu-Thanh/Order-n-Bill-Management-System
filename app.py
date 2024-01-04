94d7c6fe41b57215c87aaef8f894474dea0b5438
Bin289 <82301249+NTN014@users.noreply.github.com>
2024-01-04 20:22:25 +0700

    new update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from config import app_config, Config
 from flask_restful import Api
 from app.api.blueprints import diner_api, menu_api, order_api
@@ -11,12 +11,26 @@ app.config['FIREBASE_ADMIN'] = firebase_admin_instance
 app.register_blueprint(menu_api.menu_api_bp, url_prefix='/api/menu')
 app.register_blueprint(diner_api.diner_api_bp, url_prefix='/api/auth')
 app.register_blueprint(order_api.order_api_bp, url_prefix='/api/orders')
+from app.models import MenuItem, Diner
 
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return render_template("index.html")
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    menu_items = MenuItem.list_items()
+    return render_template('home.html', menu_items=menu_items)
 
+@app.route('/menu/<menu_item_id>')
+def get_menu_item(menu_item_id):
+    menu_item = MenuItem.get_item(menu_item_id)
+    return render_template('menu_item.html', menu_item=menu_item)
+
+@app.route('/diner/<diner_id>')
+def get_diner(diner_id):
+    # Logic to fetch diner info by ID and render diner template
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
