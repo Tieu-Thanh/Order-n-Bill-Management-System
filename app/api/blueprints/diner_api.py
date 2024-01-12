10f2c67e32ea4a09dd7804191624b633f9c2b169
Nguyen Pham Phu Thinh <105217439+Tieu-Thanh@users.noreply.github.com>
2024-01-12 17:55:28 +0700

    update diner

--- app/api/blueprints/diner_api.py
+++ app/api/blueprints/diner_api.py
@@ -5,5 +5,5 @@ from app.api.resources.diner_resource import DinerResource
 diner_api_bp = Blueprint('diner_api', __name__)
 api = Api(diner_api_bp)
 
-api.add_resource(DinerResource, '/diners/register', endpoint='diner_registration')
+api.add_resource(DinerResource, '/', endpoint='diners')
 
